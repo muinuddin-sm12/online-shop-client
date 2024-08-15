9ac7e0ff527b23fff92e164adc2ff47ee2696daf
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2024-08-15 23:23:26 +0400

    basic setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/Router";
+
+createRoot(document.getElementById("root")).render(
+  <div className="max-w-[1536px] mx-auto ">
+    <RouterProvider router={router} />
+  </div>
+);
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../../components/Banner"
+import Product from "../../components/Product"
+
+const Home = () => {
+  return (
+    <div>
+        <Banner/>
+        <Product/>
+    </div>
+  )
+}
+
+export default Home
